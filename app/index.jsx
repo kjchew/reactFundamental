37c06df03555f805cf23687518c3e573cf1aa228
Kar Joon Chew <chewkj@gmail.com>
2017-03-22 22:23:59 +0800

    Properties and State

--- app/index.jsx
+++ app/index.jsx
@@ -3,29 +3,55 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 //var ReactDOM = require('react-dom)
 
+class Profile extends React.Component{
+    constructor (props){
+        super(props)
+    }
+    render (){
+        let hobbies = this.props.hobbies.map(hobby => {
+            return (<li>{hobby}</li>)
+        })
+        return (
+            <div>
+                <section>
+                    <h3>Profile</h3>
+                    <p>Name: {this.props.name} age: {this.props.age} {this.props.bio}</p>
+                    <ul>{hobbies}</ul>
+                </section>
+
+            </div>
+        )
+    }
+}
 class App extends React.Component{
     constructor(props){
         super(props)
         this.state = {
             profiles: [
-                {name: 'David'}, {name: 'Sarah'}
+                {name: 'David', age: 30, hobbies: ['swimming', 'jogging'], bio: 'Enjoy sport'}, 
+                {name: 'Sarah', age: 32, hobbies: ['reading', 'gardening', 'parenting'], bio: 'Enjoy golden peaceful time'}
             ]
         }
     }
 
     render (){
+        let profile1 = this.state.profiles[0],
+        profile2 = this.state.profiles[1]
+        let profiles = this.state.profiles.map(profile => {
+            return (
+                <Profile
+                    name = {profile.name}
+                    age = {profile.age}
+                    bio = {profile.bio}
+                    hobbies = {profile.hobbies}
+                />
+            )
+        })
         return (
             <div>
-                <section>
-                    <h3>Profile 1</h3>
-                    <p>Name: {this.state.profiles[0].name}</p>
-                </section>
-                <section>
-                    <h3>Profile 2</h3>
-                    <p>Name: {this.state.profiles[1].name}</p>
-                </section>
-
+                {profiles}
             </div>
+
         )
         
     }

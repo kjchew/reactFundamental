9586620a20d10053ed1176d4578c8017e16259fd
Kar Joon Chew <chewkj@gmail.com>
2017-03-22 21:53:57 +0800

    getInitialState with ES6 structure

--- app/index.jsx
+++ app/index.jsx
@@ -3,28 +3,32 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 //var ReactDOM = require('react-dom)
 
-var App = React.createClass({
-    getInitialState: function(){
-        return {
+class App extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
             profiles: [
                 {name: 'David'}, {name: 'Sarah'}
             ]
         }
-    }, 
-    render: function() {
+    }
+
+    render (){
         return (
             <div>
                 <section>
                     <h3>Profile 1</h3>
-                    <p>Name: {this.state.profiles[0].name} </p>
+                    <p>Name: {this.state.profiles[0].name}</p>
                 </section>
                 <section>
                     <h3>Profile 2</h3>
-                    <p>Name: {this.state.profiles[0].name}</p>
+                    <p>Name: {this.state.profiles[1].name}</p>
                 </section>
+
             </div>
         )
+        
     }
-})
+}
 
 ReactDOM.render(<App/>, document.getElementById('app'))

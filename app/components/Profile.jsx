55fc82498c0b84246ce9d61352b2996c1ec9a66c
Chew, Kar Joon <kar.joon.chew@merck.com>
2017-03-24 20:53:19 +0800

    stateless functional component

--- app/components/Profile.jsx
+++ app/components/Profile.jsx
@@ -1,21 +1,12 @@
 import React from 'react'
-export default class Profile extends React.Component {
-    // constructor(props){
-    //     super(props)
-    // }
-    render() {
-        let hobbies = this.props.hobbies.map(hobby => {
-            return (<li>{hobby}</li>)
-        })
-        return (
-            <div>
-                <section>
-                    <h3>Profile</h3>
-                    <p>Name: {this.props.name} age: {this.props.age} {this.props.bio}</p>
-                    <ul>{hobbies}</ul>
-                </section>
-
-            </div>
-        )
-    }
-}
+let Profile = props => (
+    <div>
+        <h3>{props.name}</h3>
+        <p>Age: {props.age}</p>
+        <p>Bio: {props.bio}</p>
+        <ul>
+            {props.hobbies.map(hobby => <li>{hobby}</li>)}
+        </ul>
+    </div>
+)
+export default Profile

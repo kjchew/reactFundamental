1e643c5b0301d403baee2215caaa7b0c9404a157
Chew, Kar Joon <kar.joon.chew@merck.com>
2017-03-27 12:48:28 +0800

    higher order component

--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,63 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 //var ReactDOM = require('react-dom)
-import App from './components/App.jsx'
+
+let HOCGen = (Component, state) => class extends React.Component {
+    constructor(props){
+        super(props)
+        this.state=state
+    }
+
+    componentDidMount(){
+        setInterval(() => {
+            this.setState({
+                count: this.state.count + 1
+            })
+            
+        }, 500)
+    }    
+    render() {
+        return <Component {...this.props} {...this.state} />
+    }
+}
+
+class Comp1 extends React.Component{
+    render(){
+        return (
+            <div>
+                <p>Comp1</p>
+                {this.props.count}
+            </div>
+        )
+    }
+}
+
+class Comp2 extends React.Component{
+    render(){
+        return (
+            <div>
+                <p>Comp2</p>
+                {this.props.count}
+            </div>
+        )
+    }
+}
+let WrappedComp1 = HOCGen(Comp1, {count: 0})
+let WrappedComp2 = HOCGen(Comp2, {count: 100})
+
+class App extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return (
+            <div>
+                <WrappedComp1 />
+                <WrappedComp2 />
+            </div>
+        )
+    }
+}
 
 ReactDOM.render(<App/>, document.getElementById('app'))
